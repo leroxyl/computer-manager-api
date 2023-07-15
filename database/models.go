ba22facdbfa1e150984c0ee0587409d5a83b9863
Roxana Meixner <roxana.meixner@ubirch.com>
2023-07-15 16:26:19 +0200

    add database table constrains for empty strings in required columns

--- database/models.go
+++ database/models.go
@@ -1,9 +1,9 @@
 package database
 
 type Computer struct {
-	MACAddr      string `json:"macAddr" gorm:"primaryKey"`
-	ComputerName string `json:"computerName" gorm:"not null"`
-	IPAddr       string `json:"ipAddr" gorm:"not null"`
+	MACAddr      string `json:"macAddr" gorm:"primaryKey;check:mac_addr_required,mac_addr <> ''"`
+	ComputerName string `json:"computerName" gorm:"not null;check:computer_name_required,computer_name <> ''"`
+	IPAddr       string `json:"ipAddr" gorm:"not null;check:ip_addr_required,ip_addr <> ''"`
 	EmployeeAbbr string `json:"employeeAbbr"`
 	Description  string `json:"description"`
 }
